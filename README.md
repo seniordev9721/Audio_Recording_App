179166c4798218245ab4defbf6a09ba815d47a85
thomasheathcote <thomasheathcote@hotmail.com>
2023-08-10 03:38:07 +0900

    create react native expo app for recording app

---
+++ README.md
@@ -0,0 +1,9 @@
+# Expo Audio Recording Example
+
+This is an example cross-platform app implementing Expo's [Audio API](https://docs.expo.io/versions/latest/sdk/audio/) (among a few others, including File System, Asset, and Permissions APIs).
+
+Check out the good stuff in [the App.tsx file](https://github.com/expo/audio-recording-example/blob/master/App.tsx)!
+
+### Please report any issues at the [main Expo repository](https://github.com/expo/expo/issues)
+
+![iOS screenshot](./audio-recorder-ios.png "iOS screenshot")
---
+++ components/Icons.tsx
@@ -0,0 +1,55 @@
+import Icon from "./Icon";
+
+export const RECORD_BUTTON = new Icon(
+  require("../assets/images/record_button@1x.png"),
+  70,
+  119
+);
+export const RECORDING = new Icon(
+  require("../assets/images/record_icon@1x.png"),
+  20,
+  14
+);
+
+export const PLAY_BUTTON = new Icon(
+  require("../assets/images/play_button@1x.png"),
+  34,
+  51
+);
+export const PAUSE_BUTTON = new Icon(
+  require("../assets/images/pause_button@1x.png"),
+  34,
+  51
+);
+export const STOP_BUTTON = new Icon(
+  require("../assets/images/stop_button@1x.png"),
+  22,
+  22
+);
+
+export const MUTED_BUTTON = new Icon(
+  require("../assets/images/muted_button@1x.png"),
+  67,
+  58
+);
+export const UNMUTED_BUTTON = new Icon(
+  require("../assets/images/unmuted_button@1x.png"),
+  67,
+  58
+);
+
+export const TRACK_1 = new Icon(
+  require("../assets/images/track_1@1x.png"),
+  166,
+  5
+);
+export const THUMB_1 = new Icon(
+  require("../assets/images/thumb_1@1x.png"),
+  18,
+  19
+);
+export const THUMB_2 = new Icon(
+  require("../assets/images/thumb_2@1x.png"),
+  15,
+  19
+);
